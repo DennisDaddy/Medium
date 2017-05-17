66bb84bf9dc33fdf02c89d3872db56dfd997706f
DennisDaddy <softteez95@gmail.com>
2017-05-17 08:53:37 +0300

    try removing error

--- spec/features/user_signin_spec.rb
+++ spec/features/user_signin_spec.rb
@@ -18,6 +18,16 @@ RSpec.feature "User signing in" do
 		expect(page).to have_content @user.email
 	end
 
-	
+	scenario "unsuccessfully" do
+		visit new_user_session_path
+		
+		fill_in "Email", with: "wrongemail"
+		fill_in "Password", with: "wrongpassword"
+		
+		click_on "Log in"
+
+		expect(page).not_to have_content @user.email
+	end
+
 		
 end
