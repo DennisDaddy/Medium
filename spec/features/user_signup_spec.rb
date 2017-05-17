ccf0ff2369ddd6afd5a35e98cf461c3e170b5aa1
DennisDaddy <softteez95@gmail.com>
2017-05-17 08:21:36 +0300

    write unsuccessfull signup test

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -12,6 +12,15 @@ RSpec.describe "User signing up" do
 		expect(page).to have_content "example@gmail.com"
 	end
 
-	scenario "unsuccessfully"
+	scenario "unsuccessfully" do
+		visit new_user_registration_path
+		
+		fill_in "Email", with: "example@gmail.com"
+		fill_in "Password", with: ""
+		fill_in "Password confirmation", with: ""
+		click_on "Sign up"
+
+		expect(page).to have_content "error"
+	end
 		
 end
